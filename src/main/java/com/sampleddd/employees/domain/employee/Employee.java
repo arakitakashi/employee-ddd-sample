29de4122505dcb4659c107e300dd3bc389a4f6e6
Araki Takashi <ellaydelgado@gmail.com>
2024-03-12 22:51:32 +0900

    EmploeeクラスのIDの型変更及びIDのテスト追加

--- src/main/java/com/sampleddd/employees/domain/employee/Employee.java
+++ src/main/java/com/sampleddd/employees/domain/employee/Employee.java
@@ -5,13 +5,17 @@ import static java.util.Objects.isNull;
 
 /**
  * 従業員エンティティを表すクラス。従業員の基本情報である名前、姓、ID を保持します。
- *
- * @param id        従業員ID
- * @param firstName 従業員の名前
- * @param lastName  従業員の姓
  */
-public record Employee(int id, String firstName, String lastName) {
+public record Employee(long id, String firstName, String lastName) {
+    /**
+     * 従業員エンティティのコンストラクタ。
+     *
+     * @param id        従業員ID
+     * @param firstName 従業員の名前
+     * @param lastName  従業員の姓
+     */
     public Employee {
+        if (id <= 0) throw new IllegalArgumentException("Employee ID must be positive");
         if (isBlank(firstName)) throw new IllegalArgumentException("First name must not be blank");
         if (isBlank(lastName)) throw new IllegalArgumentException("Last name must not be blank");
     }
--- src/test/java/com/sampleddd/employees/domain/employee/EmployeeTest.java
+++ src/test/java/com/sampleddd/employees/domain/employee/EmployeeTest.java
@@ -6,20 +6,21 @@ import org.junit.jupiter.params.provider.CsvSource;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class EmployeeTest {
-    @ParameterizedTest(name = "{3}の場合")
+    @ParameterizedTest(name = "{4}の場合")
     @CsvSource(delimiter = '|', textBlock = """
-        # FIRST NAME | LAST NAME | MESSAGE                      | TESTNAME
-                     |    YAMADA | First name must not be blank | 名前がnull
-                  '' |    YAMADA | First name must not be blank | 名前が空
-                '  ' |    YAMADA | First name must not be blank | 名前がスペースのみ
-                TARO |           | Last name must not be blank  | 名字がnull
-                TARO |        '' | Last name must not be blank  | 名字が空
-                TARO |      '  ' | Last name must not be blank  | 名字がスペースのみ
+        # ID | FIRST NAME | LAST NAME | MESSAGE                      | TESTNAME
+          -1 |            |    YAMADA | Employee ID must be positive | IDがマイナス
+           0 |            |    YAMADA | Employee ID must be positive | IDが0 
+           1 |            |    YAMADA | First name must not be blank | 名前がnull
+           1 |         '' |    YAMADA | First name must not be blank | 名前が空
+           1 |       '  ' |    YAMADA | First name must not be blank | 名前がスペースのみ
+           1 |       TARO |           | Last name must not be blank  | 名字がnull
+           1 |       TARO |        '' | Last name must not be blank  | 名字が空
+           1 |       TARO |      '  ' | Last name must not be blank  | 名字がスペースのみ
         """)
-    void ガード条件に違反する不正な値を検出する(String firstName, String lastName, String message, String testName)
+    void ガード条件に違反する不正な値を検出する(long id, String firstName, String lastName, String message, String testName)
         throws Exception {
         // assert
-        int id = 1;
         assertThatThrownBy(() -> new Employee(id, firstName, lastName))
             .isInstanceOf(IllegalArgumentException.class)
             .hasMessageContaining(message);
