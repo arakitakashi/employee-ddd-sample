b63a7b0975f8c33deaf2ed4b8a8c0981564a0f66
Araki Takashi <ellaydelgado@gmail.com>
2024-03-12 22:39:38 +0900

    EmployeeTestにテストケース追加

--- src/test/java/com/sampleddd/employees/domain/employee/EmployeeTest.java
+++ src/test/java/com/sampleddd/employees/domain/employee/EmployeeTest.java
@@ -8,8 +8,13 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 public class EmployeeTest {
     @ParameterizedTest(name = "{3}の場合")
     @CsvSource(delimiter = '|', textBlock = """
-        # FIRST NAME | LAST NAME | MESSAGE            | TESTNAME
+        # FIRST NAME | LAST NAME | MESSAGE                      | TESTNAME
                      |    YAMADA | First name must not be blank | 名前がnull
+                  '' |    YAMADA | First name must not be blank | 名前が空
+                '  ' |    YAMADA | First name must not be blank | 名前がスペースのみ
+                TARO |           | Last name must not be blank  | 名字がnull
+                TARO |        '' | Last name must not be blank  | 名字が空
+                TARO |      '  ' | Last name must not be blank  | 名字がスペースのみ
         """)
     void ガード条件に違反する不正な値を検出する(String firstName, String lastName, String message, String testName)
         throws Exception {
